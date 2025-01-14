3eb6861acf38a457e10965d3132118b9f542f8c9
eliasmurcray <eliasmurcray@gmail.com>
2025-01-13 16:32:12 -0800

    Add renderer

--- src/main.c
+++ src/main.c
@@ -2,8 +2,8 @@
 #include <SDL3_ttf/SDL_ttf.h>
 #include <stdio.h>
 
-#define WINDOW_WIDTH 500
-#define WINDOW_HEIGHT 300
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 500
 
 int main() {
   if (!SDL_Init(SDL_INIT_VIDEO) || !TTF_Init()) {
@@ -11,9 +11,9 @@ int main() {
                  "Couldn't initialize SDL or SDL_ttf: %s", SDL_GetError());
     return 1;
   }
-
-  SDL_Window* window = SDL_CreateWindow("Discrub", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_HIGH_PIXEL_DENSITY);
-
+  SDL_Window* window = SDL_CreateWindow("Discrub", WINDOW_WIDTH, WINDOW_HEIGHT,
+                                        SDL_WINDOW_HIGH_PIXEL_DENSITY);
+  SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
   unsigned int running = 1;
   SDL_Event event;
   while (running) {
@@ -24,6 +24,9 @@ int main() {
           break;
       }
     }
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
   }
   TTF_Quit();
   SDL_DestroyWindow(window);
